fed95c095465cdb3786ad3631c70601e9fe4be70
bruno pagan <brunopagan1909@gmail.com>
2026-08-11 08:12:18 -0300

    adiciona connectionfactory e .env.example

---
+++ src/main/java/util/ConnectionFactory.java
@@ -0,0 +1,19 @@
+package util;
+
+import java.sql.Connection;
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    Dotenv dotenv = Dotenv.load();
+
+    private final String url =  dotenv.get("url");
+    private final String user = dotenv.get("user");
+    private final String password = dotenv.get("password");
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
+}
