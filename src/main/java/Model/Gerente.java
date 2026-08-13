b04c8b92efb47ab24a95d7ab5ebf61f66fe332e5
AlunoHenryRodrigues <henry.20260114@institutojef.org.br>
2026-08-13 20:40:44 -0300

    feat: first commit

---
+++ src/main/java/Model/Gerente.java
@@ -0,0 +1,12 @@
+package Model;
+import lombok.*;
+@Getter
+@Setter
+
+public class Gerente {
+    private int email;
+    private String nome;
+    private String senha;
+    private String emailLoja;
+
+}
