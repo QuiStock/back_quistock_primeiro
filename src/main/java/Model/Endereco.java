73d6e67887ad18acf0413951ce70785de0b9b4bc
bruno pagan <brunopagan1909@gmail.com>
2026-08-14 12:40:25 -0300

    feat: Adiciona Model e DAO de endereco

--- src/main/java/Model/Endereco.java
+++ src/main/java/Model/Endereco.java
@@ -1,20 +1,16 @@
 package Model;
 
+import lombok.*;
+
+@Getter
+@Setter
+
 public class Endereco {
     private int id;
     private String cep;
+    private String pais;
     private String rua;
     private int numero;
     private String estado;
     private String cidade;
-
-    public Endereco(String cep, String rua, int numero, String estado, String cidade) {
-        this.cep = cep;
-        this.rua = rua;
-        this.numero = numero;
-        this.estado = estado;
-        this.cidade = cidade;
-    }
-
-
 }
