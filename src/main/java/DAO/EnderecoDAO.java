d126a479b8292ae20dda36e0cc5315c22f5d9259
bruno pagan <brunopagan1909@gmail.com>
2026-08-14 23:13:02 -0300

    feat: adiciona admindDAO, bugFix: Corrige update de enderecoDAO

--- src/main/java/DAO/EnderecoDAO.java
+++ src/main/java/DAO/EnderecoDAO.java
@@ -48,18 +48,20 @@ public class EnderecoDAO {
         }
     }
 
-    public void update(Endereco endereco) throws SQLException {
+    public void update(Endereco end) throws SQLException {
         String query = "UPDATE endereco set cep = ?, pais = ?, rua = ?, numero = ?, estado = ?, cidade = ?  WHERE id = ?;";
+
+        int id =  searchEndereco(end);
 
         try (Connection conn = factory.getConnection();
         PreparedStatement stmt = conn.prepareStatement(query)){
-            stmt.setString(1, endereco.getCep());
-            stmt.setString(2, endereco.getPais());
-            stmt.setString(3, endereco.getRua());
-            stmt.setInt(4, endereco.getNumero());
-            stmt.setString(5, endereco.getCidade());
-            stmt.setString(6, endereco.getEstado());
-            stmt.setInt(7, endereco.getId());
+            stmt.setString(1, end.getCep());
+            stmt.setString(2, end.getPais());
+            stmt.setString(3, end.getRua());
+            stmt.setInt(4, end.getNumero());
+            stmt.setString(5, end.getEstado());
+            stmt.setString(6, end.getCidade());
+            stmt.setInt(7, id);
 
             stmt.executeUpdate();
         }
@@ -68,9 +70,11 @@ public class EnderecoDAO {
     public void delete(Endereco endereco) throws SQLException {
         String query = "delete from endereco where id = ?;";
 
+        int  id =  searchEndereco(endereco);
+
         try (Connection conn = factory.getConnection();
         PreparedStatement stmt = conn.prepareStatement(query)){
-            stmt.setInt(1, endereco.getId());
+            stmt.setInt(1, id);
 
             stmt.executeUpdate();
         }
