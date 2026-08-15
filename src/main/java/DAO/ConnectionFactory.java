2032a0fb0dfedf58499f75194f4d040b65e11f0e
bruno pagan <brunopagan1909@gmail.com>
2026-08-14 23:25:36 -0300

    styÃle: Adiciona comentários na connectionfactory e aplica lombok na pedido

--- src/main/java/DAO/ConnectionFactory.java
+++ src/main/java/DAO/ConnectionFactory.java
@@ -5,13 +5,16 @@ import io.github.cdimascio.dotenv.Dotenv;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+//Classe que cria as conexões
 public class ConnectionFactory {
+    //Ler informações do .env
     Dotenv dotenv = Dotenv.load();
 
     private final String url =  dotenv.get("url");
     private final String user = dotenv.get("user");
     private final String password = dotenv.get("password");
 
+    //retornar Connection, lança exceção para ser tratada por outra classe
     public Connection getConnection() throws SQLException {
         return DriverManager.getConnection(url, user, password);
     }
--- src/main/java/Model/Pedido.java
+++ src/main/java/Model/Pedido.java
@@ -1,37 +1,12 @@
 package Model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Pedido {
     String dtPedido;
     String idItem;
     double total;
-
-    public Pedido(String dtPedido, String idItem, double total) {
-        this.dtPedido = dtPedido;
-        this.idItem = idItem;
-        this.total = total;
-    }
-
-    public String getDtPedido() {
-        return dtPedido;
-    }
-
-    public void setDtPedido(String dtPedido) {
-        this.dtPedido = dtPedido;
-    }
-
-    public String getIdItem() {
-        return idItem;
-    }
-
-    public void setIdItem(String idItem) {
-        this.idItem = idItem;
-    }
-
-    public double getTotal() {
-        return total;
-    }
-
-    public void setTotal(double total) {
-        this.total = total;
-    }
 }
