63344abf8d1652b62f8ed019a69b8ebca1340871
AlunoHenryRodrigues <henry.20260114@institutojef.org.br>
2026-08-29 10:58:51 -0300

    feat: Agora sim finalizando creates das tables com check e default

--- src/main/resources/sql/creates.sql
+++ src/main/resources/sql/creates.sql
@@ -61,64 +61,64 @@ create table if not exists gerente_regional(
 
 create table if not exists promocao(
     codigo INT generated always as identity primary key Check (codigo >= 0),
-    percentual NUMERIC NOT NULL Check (percental >= 0 and percentual < 100),
+    percentual NUMERIC NOT NULL Check (percentual >= 0 and percentual < 100),
     dt_fim DATE NOT NULL,
     dt_promo DATE NOT NULL
 );
 
+create table if not exists produto(
+    lote INT generated always as identity primary key check(lote >= 0),
+    nome varchar(100) not null,
+    preco numeric(10,2) not null check(preco >= 0),
+    validade date not null,
+    marca varchar not null,
+    linha varchar not null,
+    qtd_estoque int not null default 0 check(qtd_estoque >= 0),
+    loja_id int not null check(loja_id >= 0),
+    foreign key (loja_id) references loja(codigo)
+);
+
 create table if not exists aplica(
-    produto_lote int not null,
-    promocao_codigo int not null,
+    produto_lote int not null check(produto_lote >= 0),
+    promocao_codigo int not null check(promocao_codigo >= 0),
     foreign key (produto_lote) references produto(lote),
     foreign key (promocao_codigo) references promocao(codigo)
 );
 
-create table if not exists produto(
-    lote INT generated always as identity primary key,
-    nome varchar(100) not null,
-    preco numeric not null,
-    validade date not null,
-    marca varchar not null,
-    linha varchar not null,
-    qtd_estoque int not null,
-    loja_id int not null,
-    foreign key (loja_id) references loja(codigo)
-);
+
 
 create table if not exists historico(
-    id int generated always as identity primary key,
-    produto_lote int not null,
+    id int generated always as identity primary key check(id >= 0),
+    produto_lote int not null check(produto_lote >= 0),
     foreign key (produto_lote) references produto(lote)
 );
 
 create table if not exists item_pedido(
-    id int generated always as identity primary key,
-    quantidade int not null,
-    preco numeric not null,
-    produto_lote int not null,
+    id int generated always as identity primary key check(id >= 0),
+    quantidade int not null default 0 check(quantidade >= 0),
+    preco numeric(10,2) not null check(preco >= 0),
+    produto_lote int not null check(produto_lote >= 0),
     foreign key (produto_lote) references produto(lote)
 );
 
 create table if not exists pedido(
-    codigo int generated always as identity primary key,
+    codigo int generated always as identity primary key check(codigo >= 0),
     dt_pedido date not null,
-    item_pedido_id int not null,
+    item_pedido_id int not null check(item_pedido_id >= 0),
     foreign key (item_pedido_id) references item_pedido(id),
-    total numeric not null
+    total numeric(10,2) not null check(total >= 0)
 );
 
 create table if not exists fluxo(
-    id INT generated always as identity primary key,
+    id INT generated always as identity primary key check(id >= 0),
     tipo VARCHAR(10) not null,
-    produto_lote int not null,
+    produto_lote int not null check(produto_lote >= 0),
     foreign key (produto_lote) references produto(lote)
 );
 
 create table if not exists contem(
-    historico_id int not null,
+    historico_id int not null check(historico_id >= 0),
     fluxo_id int not null,
     foreign key (historico_id) references historico(id),
     foreign key (fluxo_id) references fluxo(id)
 );
-
-select * from endereco;
