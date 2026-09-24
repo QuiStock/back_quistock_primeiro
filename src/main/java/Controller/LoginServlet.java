65cf4ac7674fd94bf8ad705749c5930cfccd836e
brunospgn <bruno.20260038@institutojef.org.br>
2026-09-24 06:31:38 -0300

    feat: adds login validation method in LoginServlet

---
+++ src/main/java/Controller/LoginServlet.java
@@ -0,0 +1,52 @@
+package Controller;
+
+import DAO.AdminDAO;
+import DAO.GerenteDAO;
+import jakarta.servlet.annotation.WebServlet;
+
+import Model.Gerente;
+import Model.Admin;
+import lombok.*;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@WebServlet("/login")
+public class LoginServlet {
+
+    AdminDAO daoA = new AdminDAO();
+    GerenteDAO daoG = new GerenteDAO();
+
+    public boolean validate(String user, String password) throws SQLException, ClassNotFoundException {
+
+        List<Admin> admins = daoA.select();
+        List<Gerente> gerentes = daoG.read();
+
+        for (Admin admin : admins) {
+
+            if (admin.getEmail().equals(user) && admin.getSenha().equals(password)){
+
+                return true;
+
+            }
+
+        }
+
+        for (Gerente gerente : gerentes) {
+            if (gerente.getEmail().equals(user) && gerente.getSenha().equals(password)){
+
+                return true;
+
+            }
+        }
+
+        return false;
+
+    }
+}
