ebbb4a874c0e1f198d5687da65247ba82602a018
AlunoHenryRodrigues <henry.20260114@institutojef.org.br>
2026-09-25 13:02:03 -0300

    feat/fix: Gerente and GerenteServlet

---
+++ src/main/java/Model/Gerente.java
@@ -0,0 +1,14 @@
+package Model;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Gerente {
+    private int codigo;
+    private String email;
+    private String nome;
+    private String senha;
+    private int regiao_codigo;
+}
